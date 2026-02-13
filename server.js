4101b4a8f7159bdec5d7d6acc1df00a8267ae2e8
toasterbath0758 <toasterbath0758@gmail.com>
2026-02-13 10:19:30 +0100

    filter voor a-z toegevoegd #3

--- server.js
+++ server.js
@@ -47,11 +47,36 @@ app.set('views', './views')
 app.use(express.urlencoded({extended: true}))
 
 
+ // ######################
 // Om Views weer te geven, heb je Routes nodig
 // Maak een GET route voor de index
 app.get('/', async function (request, response) {
 
-  // Haal alle personen uit de WHOIS API op, van dit jaar, gesorteerd op naam
+  // Haal alle personen uit de WHOIS API op, van dit jaar, gesorteerd op id
+  const params = {
+
+    // Geef aan welke data je per persoon wil terugkrijgen
+    'fields': '*,squads.*',
+
+    // Combineer meerdere filters
+    'filter[squads][squad_id][tribe][name]': 'FDND Jaar 1',
+    // Filter eventueel alleen op een bepaalde squad
+    // 'filter[squads][squad_id][name]': '1I',
+    // 'filter[squads][squad_id][name]': '1J',
+    'filter[squads][squad_id][cohort]': '2526'
+  }
+  const personResponse = await fetch('https://fdnd.directus.app/items/person/?' + new URLSearchParams(params))
+
+  // En haal daarvan de JSON op
+  const personResponseJSON = await personResponse.json()
+    response.render('index.liquid', {persons: personResponseJSON.data, squads: squadResponseJSON.data
+})
+})
+  // ######################
+  // Maak een GET route voor de filter a-z
+app.get('/az', async function (request, response) {
+
+  // Haal alle personen uit de WHOIS API op, van dit jaar, gesorteerd op id
   const params = {
     // Sorteer op naam
     'sort': 'name',
@@ -78,7 +103,10 @@ app.get('/', async function (request, response) {
   // Render index.liquid uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
   // Geef ook de eerder opgehaalde squad data mee aan de view
   response.render('index.liquid', {persons: personResponseJSON.data, squads: squadResponseJSON.data})
+  response.render('index.liquid', {persons: personResponseJSON.data, squads: squadResponseJSON.data
 })
+})
+
 
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
 app.post('/', async function (request, response) {
